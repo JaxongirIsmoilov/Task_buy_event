0500a60dd825e5d459b82bd844e6f993e5ad8fd2
Jaxongir <44000309+JaxongirIsmoilov@users.noreply.github.com>
2020-08-30 15:15:54 +0500

    Add files via upload

---
+++ storage/framework/views/1f7670a0c8183c473e93cccbe217cd01228cbca5.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>
+
+<?php /**PATH /home/jaxongir/Documents/My_projects/Buy_Event/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
